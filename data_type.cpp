8fac0685d7c9a38e0b7daa1af239a2615c3cfd9c
MAYANK VISHWAKARMA <158754678+manii3m@users.noreply.github.com>
2026-01-09 12:16:36 +0000

    Day 3

---
+++ data_type.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+
+    //int means whole number without decimal point
+    int number = 42;
+    cout << "The number is: " << number << " ( This is integer Data Type )" << endl;
+
+
+    //float means decimal number upto 7 digit after decimal point
+    float b = 3.14f;
+    cout << "The float number is: " << b << " ( This is float Data Type )" << endl;
+
+
+    //double means decimal number upto 15 digit after decimal point
+    double c = 2.71828;
+    cout << "The double number is: " << c << " ( This is double Data Type )" << endl;
+
+
+    //char means single character while string means multiple character
+    char a = 'A';
+    cout << "The character is: " << a << " ( This is character Data Type )" << endl;
+
+
+    //boolean means true or false value 
+    bool isTrue = true;
+    cout << "The boolean value is: " << isTrue << " ( This is boolean Data Type )" << endl;
+
+    return 0;
+
+
+}
