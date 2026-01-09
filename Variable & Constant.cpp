fb90090e1f4e018969c53ad30ea0f212cd31f601
MAYANK VISHWAKARMA <158754678+manii3m@users.noreply.github.com>
2026-01-09 12:09:21 +0000

    day 2

---
+++ Variable & Constant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    // Define the name of the file
+    string fileName = "myfile.txt";
+    
+    cout << "File Name: " << fileName << endl;
+    
+    return 0;
+}
