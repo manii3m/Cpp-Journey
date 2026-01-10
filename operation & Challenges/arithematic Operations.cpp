c1cfc25b4d94c2eade01c2f7cbdccdef290af4dc
manii3m <manidemo33@gmail.com>
2026-01-10 23:28:47 +0530

    day 4

---
+++ operation & Challenges/arithematic Operations.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main (){
+    
+    int cups;
+    double price_per_cup, total_cost, discounted_price;
+
+    cout << "Enter the number of cups : ";
+    cin >> cups;
+
+    cout << "Enter the price per cup : ";
+    cin >> price_per_cup;
+
+    total_cost = cups * price_per_cup;
+
+    //appling 5% discount if total price is above 100
+
+    if (total_cost > 100) {
+        discounted_price = total_cost - (total_cost * 0.05);
+        cout << "Total cost after discount is : " << discounted_price << endl;
+    } else {
+        cout << "Total cost is : " << total_cost << endl;
+    }
+
+    
+
+    return 0;
+}
