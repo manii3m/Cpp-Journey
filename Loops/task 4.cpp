9533fca72597db6f35f7a8cc17a7689e131a5d70
manii3m <manidemo33@gmail.com>
2026-01-20 14:14:17 +0530

    Add tea serving loop with stop condition
    Introduced a loop that repeatedly asks the user if they want more tea and serves another cup unless the user types 'stop'. Added a thank you message after exiting the loop.

--- Loops/task 4.cpp
+++ Loops/task 4.cpp
@@ -7,6 +7,20 @@ int main (){
 
     string response ;
 
+    while ( true ) {
+        cout << " Do you want more tea ( type 'stop' to end ) : " ;
+        getline ( cin , response );
+
+        if ( response == "stop" ) {
+            //exit the loop 
+            break;
+        }
+        cout << " Here is your another cup of tea ! " << endl;
+    }
+
+
+    cout << " Thank you ! Visit Again ! " << endl;
+
     return 0;
    
 }
