a7b0231875b3d3ce9fecefda8671e8c84f4dae9b
manii3m <manidemo33@gmail.com>
2026-01-22 17:56:59 +0530

    Add tea types loop with out-of-stock message
    Introduced an array of tea types and a loop to brew each type, skipping 'Green Tea' with an out-of-stock message.

--- Loops/task 5.cpp
+++ Loops/task 5.cpp
@@ -5,7 +5,18 @@
 using namespace std;
 int main (){
 
+    string TeaTypes[5] = { "Oolong Tea", "Black Tea", "Green Tea", "White Tea", "Herbal Tea" };
+    
+    for  (int i = 0 ; i < 5 ; i ++){
 
+        if ( TeaTypes[i] == "Green Tea") {
+            cout << " Sorry, we are out of Green Tea. " << endl;
+            continue;
+        }
+
+        cout << " Brewing your " << TeaTypes[i]  << " ..." << endl;
+    }
+    
     return 0;
    
 }
